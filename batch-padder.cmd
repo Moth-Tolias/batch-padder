49f4cd202a51c292357e269d834638acf00f1a3e
Su <moth.tolias@gmail.com>
2016-03-13 17:31:28 +0000

    complete interface code

--- batch-padder.cmd
+++ batch-padder.cmd
@@ -12,21 +12,39 @@ SET parent=%~dp0
 
 REM global vars
 SET digits=%1
-SET KeepName=%2
+SET NewName=%2
 SET extension=%3
 SET dir=%4
 
 REM default values
+IF "%digits%"=="" ( 
+	ECHO How many digits would you like to use? 
+	SET /p digits= 
+	)
 IF "%digits%"=="" ( SET digits=3 )
-IF "%KeepName%"=="" ( SET KeepName=1 )
-IF "%extension%"=="" ( SET /p extension= What extension would you like to search for? )
+
+IF "%NewName%"=="" ( 
+	ECHO Please enter a prefix.&ECHO Alternatively, press ENTER to use the original.
+	SET /p NewName= 
+	)
+
+IF "%extension%"=="" ( 
+	ECHO What extension would you like to search for? 
+	SET /p extension=
+	)
 IF "%extension%"=="" ( SET extension=bmp )
 
+REM Directory code
+IF "%dir%"=="" ( CHOICE /m Would you like to enter a folder?
+	IF %ERRORLEVEL% < 2 ( GOTO :Changedir ) REM ////////////////////////////////////
+	SET /p dir= Please specify the folder to enter. 
 
-REM change directory if specified
+:Changedir
 IF "%dir%"=="" ( GOTO :Mainloop )
 cd %dir%
+REM error check this? ////////////////////////////////////
  
 :Mainloop
 FOR %%I IN (*.%extension%) DO ECHO %%I 
+REM add a check for the prefix not being set and skip the prefix code is all we need 
 PAUSE
